e0dee4acba0a5799b16100c40f5b24747f211d78
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-06 14:09:28 +0530

    Adding blank Service project

---
+++ MyPersonalAccountsService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using com.techphernalia.MyPersonalAccounts;
+namespace com.techphernalia.MyPersonalAccounts.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MyPersonalAccountsService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
