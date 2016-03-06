1669a0dde5f4c5fe1927048fe9f9f2f67163269f
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-06 14:10:34 +0530

    Adding blank Manager project

---
+++ MyPersonalAccountsManager/Inventory/frmStockUnitManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace com.techphernalia.MyPersonalAccounts.Manager.Inventory
+{
+    public partial class frmStockUnitManager : Form
+    {
+        public frmStockUnitManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
