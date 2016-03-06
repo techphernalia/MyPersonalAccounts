709db29a52bb8928ae26c81ab4492e53d3ed574c
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-06 16:13:29 +0530

    Provide comment for StockUnit and override ToString()

--- MyPersonalAccountsModel/Inventory/StockUnit.cs
+++ MyPersonalAccountsModel/Inventory/StockUnit.cs
@@ -5,11 +5,38 @@ using System.Text;
 
 namespace com.techphernalia.MyPersonalAccounts.Model.Inventory
 {
+    /// <summary>
+    /// Unit for Inventory Items
+    /// </summary>
     public class StockUnit
     {
+        /// <summary>
+        /// Unique Identifier of Unit
+        /// </summary>
         public int StockUnitId { get; set; }
+
+        /// <summary>
+        /// Name of Unit
+        /// </summary>
         public string StockUnitName { get; set; }
+
+        /// <summary>
+        /// Symbol for Unit
+        /// </summary>
         public string StockUnitSymbol { get; set; }
+
+        /// <summary>
+        /// Number of Decimal Places in Unit
+        /// </summary>
         public int StockUnitDecimalPlaces { get; set; }
+
+        /// <summary>
+        /// Display Unit information
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("[{0}] with symbol [{1}] having {2} Decimal Places", StockUnitName, StockUnitSymbol, StockUnitDecimalPlaces);
+        }
     }
 }
