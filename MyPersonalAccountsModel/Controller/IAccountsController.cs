0e270248751ebd3883200aa464063b46a5748181
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-06 18:37:23 +0530

    Creating contract for AccoutnsController LedgerGroups

---
+++ MyPersonalAccountsModel/Controller/IAccountsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.techphernalia.MyPersonalAccounts.Model.Controller
+{
+    public interface IAccountsController
+    {
+        #region Ledger Groups
+
+        int AddLedgerGroup(LedgerGroup ledgerGroup);
+
+        void UpdateLedgerGroup(LedgerGroup ledgerGroup);
+
+        List<LedgerGroup> GetAllLedgerGroups();
+
+        LedgerGroup GetLedgerGroupById(int ledgerGroupId);
+
+        List<LedgerGroup> GetLedgerGroupsForParentGroup(int parentLedgerGroupId);
+
+        void DeleteLedgerGroup(LedgerGroup ledgerGroup);
+
+        #endregion
+    }
+}
