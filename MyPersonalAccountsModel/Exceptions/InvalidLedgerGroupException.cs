efdc437e6fdfadc9159e5fa8429f4e9fd5c63b13
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-06 18:36:09 +0530

    Adding InvalidLedgerGroupException

---
+++ MyPersonalAccountsModel/Exceptions/InvalidLedgerGroupException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace com.techphernalia.MyPersonalAccounts.Model.Exceptions
+{
+    public class InvalidLedgerGroupException : Exception
+    {
+
+        public InvalidLedgerGroupException() : base() { }
+
+        public InvalidLedgerGroupException(string message) : base(message) { }
+
+        public InvalidLedgerGroupException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected InvalidLedgerGroupException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
