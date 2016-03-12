805b2851ba654226bcabf2cc9ecf7fed9b2ee3ba
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-12 19:29:02 +0530

    Add Attributes for DataGridView

--- MyPersonalAccountsModel/Inventory/StockUnit.cs
+++ MyPersonalAccountsModel/Inventory/StockUnit.cs
@@ -1,4 +1,6 @@
-﻿namespace com.techphernalia.MyPersonalAccounts.Model.Inventory
+﻿using System.ComponentModel;
+
+namespace com.techphernalia.MyPersonalAccounts.Model.Inventory
 {
     /// <summary>
     /// Unit for Inventory Items
@@ -8,26 +10,31 @@
         /// <summary>
         /// Unique Identifier of Unit
         /// </summary>
+        [Browsable(false)]
         public int StockUnitId { get; set; }
 
         /// <summary>
         /// Name of Unit
         /// </summary>
+        [DisplayName("Unit")]
         public string StockUnitName { get; set; }
 
         /// <summary>
         /// Symbol for Unit
         /// </summary>
+        [DisplayName("Symbol")]
         public string StockUnitSymbol { get; set; }
 
         /// <summary>
         /// Number of Decimal Places in Unit
         /// </summary>
+        [DisplayName("Decimal Places")]
         public int StockUnitDecimalPlaces { get; set; }
 
         /// <summary>
         /// Human Readable System Id
         /// </summary>
+        [DisplayName("Identifier")]
         public string SystemId { get; set; }
 
         /// <summary>
