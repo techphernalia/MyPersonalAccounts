437031fe3f3031c54750cd6becd9d63061fce367
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-12 18:40:02 +0530

    Adding Model for Name Configuration

---
+++ MyPersonalAccountsModel/AppConfig/NameConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.techphernalia.MyPersonalAccounts.Model.AppConfig
+{
+    /// <summary>
+    /// Configuration of Names (for System Id)
+    /// </summary>
+    public class NameConfiguration
+    {
+        /// <summary>
+        /// Unique Identifier of Configuration
+        /// </summary>
+        public int ConfigurationId { get; set; }
+
+        /// <summary>
+        /// Name of Configuration
+        /// </summary>
+        public string SystemName { get; set; }
+
+        /// <summary>
+        /// Prefix for System ID
+        /// </summary>
+        public string Prefix { get; set; }
+
+        /// <summary>
+        /// Suffix for System ID
+        /// </summary>
+        public string Suffix { get; set; }
+
+        /// <summary>
+        /// Length of Item ID to be used
+        /// </summary>
+        public int IDLength { get; set; }
+    }
+}
