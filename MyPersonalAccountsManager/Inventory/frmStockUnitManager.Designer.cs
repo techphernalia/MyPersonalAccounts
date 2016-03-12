6cc5b07cdb5804895063c04faa22eae973266f08
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-12 19:29:25 +0530

    Stock Units Report

--- MyPersonalAccountsManager/Inventory/frmStockUnitManager.Designer.cs
+++ MyPersonalAccountsManager/Inventory/frmStockUnitManager.Designer.cs
@@ -28,11 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.dgStockUnits = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStockUnits)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(12, 9);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(815, 37);
+            this.lblHeading.TabIndex = 3;
+            this.lblHeading.Text = "Stock Units";
+            this.lblHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgStockUnits
+            // 
+            this.dgStockUnits.AllowUserToAddRows = false;
+            this.dgStockUnits.AllowUserToDeleteRows = false;
+            this.dgStockUnits.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgStockUnits.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgStockUnits.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgStockUnits.Location = new System.Drawing.Point(17, 49);
+            this.dgStockUnits.Name = "dgStockUnits";
+            this.dgStockUnits.ReadOnly = true;
+            this.dgStockUnits.Size = new System.Drawing.Size(810, 537);
+            this.dgStockUnits.TabIndex = 2;
+            // 
+            // frmStockUnitManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmStockUnitManager";
+            this.ClientSize = new System.Drawing.Size(839, 598);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.dgStockUnits);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "frmStockUnitManager";
+            this.Text = "Units";
+            this.Load += new System.EventHandler(this.frmStockUnitManager_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgStockUnits)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.DataGridView dgStockUnits;
     }
 }
--- MyPersonalAccountsManager/Inventory/frmStockUnitManager.cs
+++ MyPersonalAccountsManager/Inventory/frmStockUnitManager.cs
@@ -8,5 +8,17 @@ namespace com.techphernalia.MyPersonalAccounts.Manager.Inventory
         {
             InitializeComponent();
         }
+
+        private void frmStockUnitManager_Load(object sender, System.EventArgs e)
+        {
+            var stockUnits = ServiceInteractor.GetApplicationController().GetAllStockUnits();
+            dgStockUnits.DataSource = stockUnits;
+            dgStockUnits.Columns.Add(new DataGridViewButtonColumn { HeaderText = "Update", Text = "Update", UseColumnTextForButtonValue = true, Name = "Update", DisplayIndex = 0 });
+            dgStockUnits.Columns.Add(new DataGridViewButtonColumn { HeaderText = "Delete", Text = "Delete", UseColumnTextForButtonValue = true , Name="Delete", DisplayIndex = 1});
+        }
+
+        private void dgStockUnits_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
     }
 }
