2d5ba9d2847875cdca8dfd3fcaa752fbb935c251
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-12 17:03:12 +0530

    Adding Application Controller (Main Contract)

---
+++ MyPersonalAccountsModel/IApplicationController.cs
@@ -0,0 +1,10 @@
+﻿using com.techphernalia.MyPersonalAccounts.Model.Controller;
+using System.ServiceModel;
+
+namespace com.techphernalia.MyPersonalAccounts.Model
+{
+    [ServiceContract]
+    public interface IApplicationController : IAccountsController, IInventoryController
+    {
+    }
+}
