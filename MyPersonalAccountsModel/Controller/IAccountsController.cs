596cbd8cd9c46bf7c86eae58aa04c167dbb43e6d
Durgesh Chaudhary <dcbhai@gmail.com>
2016-03-12 17:02:37 +0530

    Adding Contract attributes to interface

--- MyPersonalAccountsModel/Controller/IAccountsController.cs
+++ MyPersonalAccountsModel/Controller/IAccountsController.cs
@@ -1,24 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace com.techphernalia.MyPersonalAccounts.Model.Controller
 {
+    [ServiceContract]
     public interface IAccountsController
     {
         #region Ledger Groups
 
+        [OperationContract]
         int AddLedgerGroup(LedgerGroup ledgerGroup);
 
+        [OperationContract]
         void UpdateLedgerGroup(LedgerGroup ledgerGroup);
 
+        [OperationContract]
         List<LedgerGroup> GetAllLedgerGroups();
 
+        [OperationContract]
         LedgerGroup GetLedgerGroupById(int ledgerGroupId);
 
+        [OperationContract]
         List<LedgerGroup> GetLedgerGroupsForParentGroup(int parentLedgerGroupId);
 
+        [OperationContract]
         void DeleteLedgerGroup(LedgerGroup ledgerGroup);
 
         #endregion
--- MyPersonalAccountsModel/Controller/IInventoryController.cs
+++ MyPersonalAccountsModel/Controller/IInventoryController.cs
@@ -1,55 +1,71 @@
 ﻿using com.techphernalia.MyPersonalAccounts.Model.Inventory;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ServiceModel;
 
 namespace com.techphernalia.MyPersonalAccounts.Model.Controller
 {
+    [ServiceContract]
     public interface IInventoryController
     {
         #region Stock Unit
 
+        [OperationContract]
         List<StockUnit> GetAllStockUnits();
 
+        [OperationContract]
         StockUnit GetStockUnitFromId(int unitId);
 
+        [OperationContract]
         int AddStockUnit(StockUnit stockUnit);
 
+        [OperationContract]
         void UpdateStockUnit(StockUnit stockUnit);
 
+        [OperationContract]
         void DeleteStockUnit(int unitId);
 
         #endregion
 
         #region Stock Group
 
+        [OperationContract]
         List<StockGroup> GetAllStockGroups();
-        
+
+        [OperationContract]
         List<StockGroup> GetStockGroupsForGroup(int stockGroupId);
-        
+
+        [OperationContract]
         StockGroup GetStockGroupFromId(int stockGroupId);
-        
+
+        [OperationContract]
         int AddStockGroup(StockGroup stockGroup);
-        
+
+        [OperationContract]
         void UpdateStockGroup(StockGroup stockGroup);
 
+        [OperationContract]
         void DeleteStockGroup(int stockGroupId);
 
         #endregion
 
         #region Stock Item
 
+        [OperationContract]
         List<StockItem> GetAllStockItems();
 
+        [OperationContract]
         List<StockItem> GetStockItemsForGroup(int stockGroupId);
 
+        [OperationContract]
         StockItem GetStockItemFromId(int stockItemId);
 
+        [OperationContract]
         int AddStockItem(StockItem stockItem);
 
+        [OperationContract]
         void UpdateStockItem(StockItem stockItem);
 
+        [OperationContract]
         void DeleteStockItem(int stockItemId);
 
         #endregion
